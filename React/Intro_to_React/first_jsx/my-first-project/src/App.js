47e231e868d56814907a0b3dfb4d50ac857f0987
Chelsey Campbell <cc.chels.campbell@gmail.com>
2023-06-07 16:35:20 -0600

    Initial commit

---
+++ React/Intro_to_React/first_jsx/my-first-project/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <h5>* Learn React</h5>
+      <h5>* Climb Mt. Everest</h5>
+      <h5>* Run a marathon</h5>
+      <h5>* Feed the dogs</h5>
+    </div>
+  );
+}
+
+export default App;
