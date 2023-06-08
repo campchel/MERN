2f363257deefce5bdcac23caf3df7a0e2eaf9625
Chelsey Campbell <cc.chels.campbell@gmail.com>
2023-06-08 16:03:10 -0600

    More Forms

---
+++ React/Lifting_State/more_forms/more-forms/src/App.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "./components/MoreFomComponent";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+      
+    </div>
+  );
+}
+
+export default App;
+
--- React/Lifting_State/more_forms/more-forms/src/components/MoreFomComponent.jsx
+++ React/Lifting_State/more_forms/more-forms/src/components/MoreFomComponent.jsx
@@ -1,46 +1,63 @@
 import React, { useState } from 'react'
 
-const Form = () => {
-    // firstName, lastName, email, password
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const[email, setEmail] = useState("")
-    const[password, setPassword] = useState("")
 
+const Form = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const [inputError, setInputError]= useState("");
+
+    const errorHandler = (e) => {
+        setInputError(e.target.value);
+        if(e.target.value.length < 1) {
+            setInputError("Input is required!");
+        }else if (e.target.value.length < 2) {
+            setInputError("Input needs to be at least 2 characters long!");
+        }else{
+            setInputError("");
+        }
+    }
     let [userInfo, setUserInfo] = useState([])
 
     const createUser = (e) => {
         e.preventDefault()
         console.log("Form submitted", firstName, lastName, email, password)
-        let userObj = {firstName,lastName, email, password}
-        setUserInfo([...userInfo, userObj])
+        const userObj = {firstName,lastName, email, password}
+        setUserInfo([...userInfo, userObj])    
     }
 
     return (
-        <div> 
-            <h1>Hook Form</h1>
+        <div>
+            <h1>More Forms</h1>
             <form onSubmit={ createUser }>
             <div className= 'form-group'>
                 <label>First Name: </label> 
-                <input type="text" onChange={ (e) => setFirstName(e.target.value) } className= 'form-control' />
+                <input type="text" onChange={errorHandler} className= 'form-control' />
+                {firstName.length>0 && firstName.length<2 ? <p>must be 2 characters</p> : ""}
             </div>
             <div className= 'form-group'>
                 <label>Last Name: </label> 
-                <input type="text" onChange={ (e) => setLastName(e.target.value) } className= 'form-control' />
+                <input type="text" onChange={errorHandler} className= 'form-control' />
             </div>
             <div className= 'form-group'>
                 <label>Email Address: </label> 
-                <input type="text" onChange={ (e) => setEmail(e.target.value) } className= 'form-control'  />
+                <input type="text" onChange={errorHandler} className= 'form-control'  />
             </div>
             <div className= 'form-group'>
                 <label>Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } className= 'form-control' />
+                <input type="text" onChange={errorHandler} className= 'form-control' />
             </div>
             <div className= 'form-group'>
                 <label>Confirm Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } className= 'form-control' />
+                <input type="text" onChange={errorHandler} className= 'form-control' />
             </div>
-            <input type="submit" value="Create User" className='btn btn-outline-dark mt-3' />
+            <div>
+            {
+                inputError ? <p>{inputError}</p> : ""
+            }
+            <input type="submit" value="Create User" className='btn btn-outline-dark mt-3'></input>
+            </div>
             </form>
             <hr />
             <div className='user-card'>
